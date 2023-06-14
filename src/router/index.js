5d3401fa5b829419b53d92017a845364c599ad8c
AryaPrayogi16 <aryaprayogi16@gmail.com>
2023-06-15 01:53:53 +0700

    nyenye

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,39 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+
+import { createRouter, createWebHistory } from 'vue-router';
+
+
 
 const routes = [
   {
     path: '/',
-    component: () => import('@/layouts/Guest.vue'),
+    component: () => import('/src/layouts/Guest.vue'),
     children: [
       {
         path: '/',
         name: 'login',
-        component: () => import('@/views/Login.vue'),
+        component: () => import('/src/views/Login.vue'),
       },
       {
         path: 'register',
         name: 'register',
-        component: () => import('@/views/Register.vue'),
+        component: () => import('/src/views/Register.vue'),
       },
     ],
   },
 
   {
     path:'/auth/home',
-    component: () => import('@/views/Home.vue'),
+    component: () => import('/src/views/Home.vue'),
     name:'home'
   },
   {
     path: '/auth/msg',
-    component: () => import('@/views/Message.vue'),
+    component: () => import('/src/views/Message.vue'),
     name:'msg.room'
   }
 ]
+
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
